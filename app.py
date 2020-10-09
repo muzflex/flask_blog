502a6c374aa9a8e86ae4ce5d637a7ee01d2a1102
muzflex <samagags@gmail.com>
2020-10-09 13:49:48 +0200

    second commit

--- app.py
+++ app.py
@@ -1,15 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Sam Magagula',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20 2020'
+    },
+    {
+        'author': 'Isaac Gama',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'May 10 2020'
+    },
+    {
+        'author': 'Thoko Ndlovu',
+        'title': 'Blog Post 3',
+        'content': 'Third post content',
+        'date_posted': 'June 20 20190'
+    }
+]
+
+
 @app.route('/')
+@app.route('/home')
 def index():
-    return "hello world"
+    return render_template("home.html",posts=posts)
 
 
-@app.route('/mike')
-def mike():
-    return "hello Mike!"
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 
 if __name__ == '__main__':
